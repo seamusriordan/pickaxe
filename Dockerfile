a5b0dce8956adc58ee18ee90f82eb07367352c24
Seamus Riordan <seamus@riordan.io>
2019-12-28 15:20:49 -0600

    All paths are relative to pickaxe

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM openjdk:12-alpine
 COPY server /home/pickaxe/server
-COPY client/build/ /home/pickaxe/html/
+COPY client/build/ /home/pickaxe/html/pickaxe
 
 WORKDIR /home/pickaxe/server/
 RUN ./gradlew build
--- cypress/integration/simple_spec.js
+++ cypress/integration/simple_spec.js
@@ -4,7 +4,7 @@ describe('simple test', () => {
     })
 
     it('can visit our page', () => {
-        cy.visit('localhost:8080')
+        cy.visit('localhost:8080/pickaxe')
         cy.get('html').contains('Make a pick')
     })
 });
