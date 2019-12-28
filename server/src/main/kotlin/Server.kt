5cc8eae99d427c5b3dd9c3424c1ac67cff2c168b
Seamus Riordan <seamus@riordan.io>
2019-12-28 15:41:45 -0600

    Separate workflow between build and deploy

--- server/src/main/kotlin/Server.kt
+++ server/src/main/kotlin/Server.kt
@@ -2,9 +2,6 @@ import io.javalin.Javalin
 import io.javalin.http.staticfiles.Location
 
 fun main(args: Array<String>) {
-    println("GHahhhhhh")
-
     var app = Javalin.create().start(8080);
     app.config.addStaticFiles("html", Location.EXTERNAL)
-//    app.get("/") { ctx -> ctx.result("make a pick") };
 }
