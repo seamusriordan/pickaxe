90a5e9d20343547be3878e16778597289d5b6325
Seamus Riordan <seamus@riordan.io>
2019-12-29 17:10:16 -0600

    Rename env variables to get https going on graphql

--- client/src/PicksGrid.js
+++ client/src/PicksGrid.js
@@ -12,9 +12,9 @@ const PicksGrid = () => {
     const {loading, error, data} = useQuery(USER_QUERY);
 
     return <div>
-        {loading ? "Loading" :
-            error ? "Error" :
-                !data ? "derp" :
+        { loading ? "Loading" :
+          error   ? "Error"   :
+         !data    ? "derp"    :
             data.users.map((user, index) => {
             return <div className="namecell" key={index}>{user.name}</div>
         })}
