2d0a8540f7e1f24e6c3be014967ca1d247b93b6e
Seamus Riordan <seamus@riordan.io>
2019-12-30 13:07:55 -0600

    Can do options and static file serving

--- server/src/main/kotlin/Server.kt
+++ server/src/main/kotlin/Server.kt
@@ -11,11 +11,33 @@ import io.javalin.Javalin
 import io.javalin.http.staticfiles.Location
 import io.javalin.plugin.json.JavalinJson.toJson
 
-fun main(args: Array<String>) {
-    val app = Javalin.create().start(8080)
-    app.config.addStaticFiles("html", Location.EXTERNAL)
 
-    app.options("/pickaxe/graphql/") { ctx ->
+fun addStaticFileServing(server: Javalin, path: String ){
+    server.config.addStaticFiles(path, Location.EXTERNAL);
+    return
+}
+
+fun addGraphQLOptionServe(server:Javalin){
+    server.options("/pickaxe/graphql/") { ctx ->
+        ctx.header("Access-Control-Allow-Origin", "*")
+        ctx.header("Access-Control-Allow-Methods", "OPTIONS, POST, GET")
+        ctx.header("Access-Control-Allow-Headers", "*")
+        ctx.header("Access-Control-Max-Age", "86400")
+    }
+    return
+}
+
+fun main(args: Array<String>) {
+    val server = Javalin.create()
+
+    addStaticFileServing(server, "html")
+    addGraphQLOptionServe(server)
+
+    server.start(8080)
+
+    return;
+
+    server.options("/pickaxe/graphql/") { ctx ->
         ctx.header("Access-Control-Allow-Origin", "*")
         ctx.header("Access-Control-Allow-Methods", "OPTIONS, POST, GET")
         ctx.header("Access-Control-Allow-Headers", "*")
@@ -35,9 +57,9 @@ fun main(args: Array<String>) {
     val typeDefinitionRegistry = schemaParser.parse(schema)
 
     val usersList = ArrayList<UserDTO>()
-        usersList.add(UserDTO("Stebe jorbs"))
-        usersList.add(UserDTO("Stebe Stebe"))
-        usersList.add(UserDTO("Dave Steve"))
+    usersList.add(UserDTO("Stebe jorbs"))
+    usersList.add(UserDTO("Stebe Stebe"))
+    usersList.add(UserDTO("Dave Steve"))
 
 
     val runtimeWiring = RuntimeWiring.newRuntimeWiring()
@@ -52,14 +74,15 @@ fun main(args: Array<String>) {
 
     val build = GraphQL.newGraphQL(graphqlSchema).build()
 
-    val mapTypeReference: MapType = TypeFactory.defaultInstance().constructMapType(HashMap::class.java, String::class.java, Any::class.java)
+    val mapTypeReference: MapType =
+        TypeFactory.defaultInstance().constructMapType(HashMap::class.java, String::class.java, Any::class.java)
 
-    app.post("/pickaxe/graphql/") { ctx ->
+    server.post("/pickaxe/graphql/") { ctx ->
         var mapper = jacksonObjectMapper();
         var query = mapper.readValue<HashMap<String, Any>>(ctx.body(), mapTypeReference)
 
         var executionInput = ExecutionInput.newExecutionInput()
-            .query( query["query"] as String).build()
+            .query(query["query"] as String).build()
 
         println(executionInput)
         var executionResult = build.execute(executionInput)
--- server/src/test/kotlin/ServerTest.kt
+++ server/src/test/kotlin/ServerTest.kt
@@ -1,10 +1,44 @@
-import org.junit.jupiter.api.Assertions.*
+import io.javalin.Javalin
+import io.javalin.core.JavalinConfig
+import io.javalin.http.staticfiles.Location
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.*
 
 internal class ServerTest {
+    val mockedServer = mock(Javalin::class.java);
+
+    @BeforeEach
+    fun beforeEach() {
+        reset(mockedServer)
+    }
 
     @Test
-    fun iAmAPassingTest() {
-        assertEquals(1,1)
+    fun addsStaticFilesWithPath() {;
+        mockedServer.config = mock(JavalinConfig::class.java);
+        val path = "some_path"
+
+        addStaticFileServing(mockedServer, path);
+
+        verify(mockedServer.config).addStaticFiles(path, Location.EXTERNAL);
+    }
+
+    @Test
+    fun addsStaticFilesWithDifferentPath() {;
+        mockedServer.config = mock(JavalinConfig::class.java);
+        val path = "html"
+
+        addStaticFileServing(mockedServer, path);
+
+        verify(mockedServer.config).addStaticFiles(path, Location.EXTERNAL);
+    }
+
+    @Test
+    fun handlesOptionsMethod() {
+        val serverSpy = spy(Javalin.create())
+
+        addGraphQLOptionServe(serverSpy);
+
+        verify(serverSpy).options(eq("/pickaxe/graphql/"), any())
     }
 }
