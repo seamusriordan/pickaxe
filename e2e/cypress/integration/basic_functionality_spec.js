888d442256cfd92745f9e133fa564f3fff031168
Seamus Riordan <seamus@riordan.io>
2020-01-01 09:32:51 -0600

    Test mutation query

--- e2e/cypress/integration/basic_functionality_spec.js
+++ e2e/cypress/integration/basic_functionality_spec.js
@@ -1,4 +1,3 @@
-
 describe('server endpoint is up', () => {
 
     it('can visit our page', () => {
@@ -11,29 +10,41 @@ describe('server endpoint is up', () => {
 describe('GraphQL server responds', () => {
 
     it('can query to our graphql endpoint', () => {
-        const graphqlRequestBody = "{\"operationName\":\"Query\",\"variables\":{},"+
+        const graphqlRequestBody = "{\"operationName\":\"Query\",\"variables\":{}," +
             "\"query\":\"query Query {\\n  users {\\n    name\\n    __typename\\n  }\\n}\\n\"}";
 
         cy.request('POST', 'localhost:8080/pickaxe/graphql', graphqlRequestBody).then(
             (response) => {
-               expect(response.status).to.equal(200)
+                expect(response.status).to.equal(200)
             }
         )
     });
 
     it('can query response follows predetermined input', () => {
-        const graphqlRequestBody = "{\"operationName\":\"Query\",\"variables\":{},"+
+        const graphqlRequestBody = "{\"operationName\":\"Query\",\"variables\":{}," +
             "\"query\":\"query Query {\\n  users {\\n    name\\n    __typename\\n  }\\n}\\n\"}";
 
         cy.request('POST', 'localhost:8080/pickaxe/graphql', graphqlRequestBody).then(
             (response) => {
-               expect(response.body).to.equal("{\"data\":{\"users\":[{\"name\":\"Seamus\",\"__typename\":\"User\"},{\"name\":\"Sereres\",\"__typename\":\"User\"},{\"name\":\"RNG\",\"__typename\":\"User\"},{\"name\":\"Vegas\",\"__typename\":\"User\"}]}}")
+                expect(response.body).to.equal("{\"data\":{\"users\":[{\"name\":\"Seamus\",\"__typename\":\"User\"},{\"name\":\"Sereres\",\"__typename\":\"User\"},{\"name\":\"RNG\",\"__typename\":\"User\"},{\"name\":\"Vegas\",\"__typename\":\"User\"}]}}")
+                expect(JSON.parse(response.body).errors).undefined
+            }
+        )
+    });
+
+    it('mutation query response has no errors', () => {
+        const graphqlRequestBody = "{\"operationName\":\"Mutation\",\"variables\":{\"name\":\"Seamus\",\"pick\":{\"game\":\"SEA@PHI\",\"pick\":\"SEA\"}},\"query\":\"mutation Mutation($name: String, $pick: UpdatedPick) {\\n  updatePick(name: $name, pick: $pick)\\n}\\n\"}";
+
+        cy.request('POST', 'localhost:8080/pickaxe/graphql', graphqlRequestBody).then(
+            (response) => {
+                expect(response.status).to.equal(200)
+                expect(JSON.parse(response.body).errors).undefined
             }
         )
     });
 
     it('has access control header *', () => {
-        const graphqlRequestBody = "{\"operationName\":\"Query\",\"variables\":{},"+
+        const graphqlRequestBody = "{\"operationName\":\"Query\",\"variables\":{}," +
             "\"query\":\"query Query {\\n  users {\\n    name\\n    __typename\\n  }\\n}\\n\"}";
 
         cy.request('POST', 'localhost:8080/pickaxe/graphql', graphqlRequestBody).then(
@@ -46,8 +57,8 @@ describe('GraphQL server responds', () => {
     it('graphql endpoint responds to OPTIONS with origin *', () => {
         cy.request('OPTIONS', 'localhost:8080/pickaxe/graphql').then(
             (response) => {
-               expect(response.status).to.equal(200);
-               expect(response.headers['access-control-allow-origin']).to.equal("*")
+                expect(response.status).to.equal(200);
+                expect(response.headers['access-control-allow-origin']).to.equal("*")
             }
         )
     });
