bbe5f0bbdfb4f4bb4072885f88efce7a4c0a9463
Seamus Riordan <seamus@riordan.io>
2020-01-01 14:24:49 -0600

    Add in data store for weekly user picks

---
+++ server/src/main/kotlin/UserPickDataQueryFetcher.kt
@@ -0,0 +1,8 @@
+import graphql.schema.DataFetcher
+import graphql.schema.DataFetchingEnvironment
+
+class UserPickDataQueryFetcher(private var store: List<List<UserPicksDTO>>) : DataFetcher<List<UserPicksDTO>> {
+    override fun get(environment: DataFetchingEnvironment?): List<UserPicksDTO> {
+        return store[0];
+    }
+}
