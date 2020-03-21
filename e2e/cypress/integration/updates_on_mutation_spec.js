4e898544608bb8aec4fae4e705d13d6298c858bc
seamus <seamus@riordan.io>
2020-03-21 15:13:09 -0500

    Add in waits so page finishes rendering before post

--- e2e/cypress/integration/updates_on_mutation_spec.js
+++ e2e/cypress/integration/updates_on_mutation_spec.js
@@ -1,19 +1,18 @@
-describe('Mutation response update',() => {
+describe('Mutation response update', () => {
     it('mutation query causes update', () => {
-        cy.visit('localhost:8080/pickaxe');
-        cy.get('#Seamus-SEA\\@PHI').contains("SEA");
+        let graphqlRequestBody = "{\"operationName\":\"Mutation\",\"variables\":{\"name\":\"Seamus\",\"week\":0,\"game\":\"SEA@PHI\",\"pick\":\"DERP\"},\"query\":\"mutation Mutation($name: String, $week: Int, $game: String, $pick: String) {\\n  updatePick(name: $name, userPick: {week: $week, game: $game, pick: $pick})\\n}\\n\"}";
 
-        let  graphqlRequestBody = "{\"operationName\":\"Mutation\",\"variables\":{\"name\":\"Seamus\",\"week\":0,\"game\":\"SEA@PHI\",\"pick\":\"DERP\"},\"query\":\"mutation Mutation($name: String, $week: Int, $game: String, $pick: String) {\\n  updatePick(name: $name, userPick: {week: $week, game: $game, pick: $pick})\\n}\\n\"}";
+        cy.visit('localhost:8080/pickaxe')
+            .get('#Seamus-SEA\\@PHI')
+            .contains("SEA");
 
-        cy.request('POST', 'localhost:8080/pickaxe/graphql', graphqlRequestBody);
-        cy.reload();
-
-        cy.get('#Seamus-SEA\\@PHI').contains("DERP");
+        cy.wait(500).request('POST', 'localhost:8080/pickaxe/graphql', graphqlRequestBody)
+            .get('#Seamus-SEA\\@PHI')
+            .contains("DERP");
 
         graphqlRequestBody = "{\"operationName\":\"Mutation\",\"variables\":{\"name\":\"Seamus\",\"week\":0,\"game\":\"SEA@PHI\",\"pick\":\"SEA\"},\"query\":\"mutation Mutation($name: String, $week: Int, $game: String, $pick: String) {\\n  updatePick(name: $name, userPick: {week: $week, game: $game, pick: $pick})\\n}\\n\"}";
-        cy.request('POST', 'localhost:8080/pickaxe/graphql', graphqlRequestBody);
-        cy.reload();
-
-        cy.get('#Seamus-SEA\\@PHI').contains("SEA");
+        cy.wait(500).request('POST', 'localhost:8080/pickaxe/graphql', graphqlRequestBody)
+            .get('#Seamus-SEA\\@PHI')
+            .contains("SEA");
     });
 });
