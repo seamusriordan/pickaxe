81a526d2b2f200e5d2c3916e10407f7999b70ad6
seamus <seamus@riordan.io>
2020-04-09 07:51:10 -0500

    Pull out destructuring of query data

--- client/src/grid/PicksGrid.js
+++ client/src/grid/PicksGrid.js
@@ -5,6 +5,21 @@ import {PICKS_QUERY, UPDATE_PICKS_MUTATION} from "../graphqlQueries";
 import PickCells from "./PickCells";
 import RowOrColumnCells from "./RowOrColumnCells"
 
+function destructureUserData(users) {
+    return {
+        names: users?.map(user => user.name),
+        totals: users?.map(user => user.total)
+    };
+}
+
+function destructureGameData(games) {
+    return {
+        names: games?.map(game => game.name),
+        spreads: games?.map(game => game.spread),
+        results: games?.map(game => game.result)
+    };
+}
+
 const PicksGrid = () => {
     const {loading, error, data, refetch} = useQuery(PICKS_QUERY, {variables: {week: "0"}, pollInterval: 600000});
     const [sendData] = useMutation(UPDATE_PICKS_MUTATION);
@@ -34,15 +49,17 @@ const PicksGrid = () => {
         }
     });
 
+    const user = destructureUserData(data?.users);
+    const game = destructureGameData(data?.games);
     return <div>
         {loading ? "Loading" : error ? "Error" : !data ? "derp" :
             [
-                <RowOrColumnCells key="name-cells" items={data.users?.map(user => user.name)} name="name"/>,
-                <RowOrColumnCells key="game-cells" items={data.games?.map(game => game.name)} name="game"/>,
-                <RowOrColumnCells key="spread-cells" items={data.games?.map(game => game.spread)} name="spread"/>,
+                <RowOrColumnCells key="name-cells" items={user.names} name="name"/>,
+                <RowOrColumnCells key="game-cells" items={game.names} name="game"/>,
+                <RowOrColumnCells key="spread-cells" items={game.spreads} name="spread"/>,
                 <PickCells key="pick-cells" data={data} sendData={sendData}/>,
-                <RowOrColumnCells key="result-cells" items={data.games?.map(game => game.result)} name="result"/>,
-                <RowOrColumnCells key="total-cells" items={data.users?.map(user => user.total)} name="total"/>
+                <RowOrColumnCells key="result-cells" items={game.results} name="result"/>,
+                <RowOrColumnCells key="total-cells" items={user.totals} name="total"/>
             ]
         }
     </div>
