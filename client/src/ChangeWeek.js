0877dca0c81ffe7f076b187adbfd3b1eb8794401
seamus <seamus@riordan.io>
2020-04-11 05:42:23 -0500

    Week UI has forward, back, and displayed week

--- client/src/ChangeWeek.js
+++ client/src/ChangeWeek.js
@@ -1,8 +1,13 @@
 import React from "react";
 
-const ChangeWeek = () => <div>
-    <div id="week-back" />
-    <div id="week-forward" />
-</div>
+const ChangeWeek = props => {
+    const {week} = props;
+
+    return <div>
+        <div id="changeWeek-back">Back</div>
+        <div id="changeWeek-week">{week}</div>
+        <div id="changeWeek-forward">Next</div>
+    </div>
+}
 
 export default ChangeWeek
--- client/src/ChangeWeek.test.js
+++ client/src/ChangeWeek.test.js
@@ -6,7 +6,7 @@ describe('ChangeWeek Widget', () => {
     let changeWeek;
 
     beforeEach(() => {
-        changeWeek = create(<ChangeWeek/>);
+        changeWeek = create(<ChangeWeek week="0"/>);
     })
 
     it('renders', () => {
@@ -14,12 +14,24 @@ describe('ChangeWeek Widget', () => {
     });
 
     it('has a forward button', () => {
-        const forwardButton = changeWeek.root.findAllByProps({id: "week-forward"})
+        const forwardButton = changeWeek.root.findAllByProps({id: "changeWeek-forward"})
         expect(forwardButton.length).toEqual(1)
     })
 
     it('has a backwards button', () => {
-        const forwardButton = changeWeek.root.findAllByProps({id: "week-back"})
-        expect(forwardButton.length).toEqual(1)
+        const backButton = changeWeek.root.findAllByProps({id: "changeWeek-back"})
+        expect(backButton.length).toEqual(1)
+    })
+
+    it('displays the week 0 from props with week 0', () => {
+        const weekElement = changeWeek.root.findByProps({id: "changeWeek-week"})
+        expect(weekElement.children[0]).toEqual("0")
+    })
+
+    it('displays the week derp from props with week derp', () => {
+        const changeWeekWith7 = create(<ChangeWeek week="derp"/>)
+
+        const weekElement = changeWeekWith7.root.findByProps({id: "changeWeek-week"})
+        expect(weekElement.children[0]).toEqual("derp")
     })
 });
