ab1633b2500190c3346a66b5ba231966c62a4b10
seamus <seamus@riordan.io>
2020-04-11 05:55:31 -0500

    Forward and back buttons call callbacks

--- client/src/ChangeWeek.js
+++ client/src/ChangeWeek.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 const ChangeWeek = props => {
-    const {week} = props;
+    const {week, forward, back} = props;
 
     return <div>
-        <div id="changeWeek-back">Back</div>
-        <div id="changeWeek-week">{week}</div>
-        <div id="changeWeek-forward">Next</div>
+        <div id="changeWeek-back" onClick={back}>Back</div>
+        <div id="changeWeek-week">{`Week ${week}`}</div>
+        <div id="changeWeek-forward" onClick={forward}>Next</div>
     </div>
 }
 
--- client/src/ChangeWeek.test.js
+++ client/src/ChangeWeek.test.js
@@ -1,12 +1,14 @@
-import {create} from "react-test-renderer";
+import {act, create} from "react-test-renderer";
 import ChangeWeek from "./ChangeWeek";
 import React from "react";
 
 describe('ChangeWeek Widget', () => {
     let changeWeek;
+    let forwardSpy = jest.fn();
+    let backSpy = jest.fn();
 
     beforeEach(() => {
-        changeWeek = create(<ChangeWeek week="0"/>);
+        changeWeek = create(<ChangeWeek week="0" forward={forwardSpy} back={backSpy}/>);
     })
 
     it('renders', () => {
@@ -25,13 +27,33 @@ describe('ChangeWeek Widget', () => {
 
     it('displays the week 0 from props with week 0', () => {
         const weekElement = changeWeek.root.findByProps({id: "changeWeek-week"})
-        expect(weekElement.children[0]).toEqual("0")
+        expect(weekElement.children[0]).toContain("0")
     })
 
     it('displays the week derp from props with week derp', () => {
-        const changeWeekWith7 = create(<ChangeWeek week="derp"/>)
+        const changeWeekWith7 = create(<ChangeWeek week="derp" forward={forwardSpy}/>)
 
         const weekElement = changeWeekWith7.root.findByProps({id: "changeWeek-week"})
-        expect(weekElement.children[0]).toEqual("derp")
+        expect(weekElement.children[0]).toContain("derp")
+    })
+
+    it('click forward calls forward callback', () => {
+        const forwardButton = changeWeek.root.findByProps({id: "changeWeek-forward"})
+
+        act(() => {
+            forwardButton.props.onClick();
+        });
+
+        expect(forwardSpy).toHaveBeenCalled();
+    })
+
+    it('click backwards calls back callback', () => {
+        const backButton = changeWeek.root.findByProps({id: "changeWeek-back"})
+
+        act(() => {
+            backButton.props.onClick();
+        });
+
+        expect(backSpy).toHaveBeenCalled();
     })
 });
