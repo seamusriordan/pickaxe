d3ea5df97d08de7443be1c8f3790e465cbf2b018
Seamus Riordan <seamus@riordan.io>
2020-04-15 06:58:41 -0500

    Refactor user weekly totals query

--- server/src/main/kotlin/db/UserWeekTotalQuery.kt
+++ server/src/main/kotlin/db/UserWeekTotalQuery.kt
@@ -1,6 +1,6 @@
 package db
 
-import dto.UserWeekTotalDTO
+import dto.*
 import graphql.schema.DataFetcher
 import graphql.schema.DataFetchingEnvironment
 import java.sql.Connection
@@ -13,22 +13,42 @@ class UserWeekTotalQuery(val connection: Connection) : DataFetcher<List<UserWeek
         val allUserPicks = UserPickQuery(connection).getPicksForWeek(week)
 
         val results = ArrayList<UserWeekTotalDTO>(0)
-
         for (user in users) {
-            val weeklyTotal = UserWeekTotalDTO(user)
-
-            val userPicks = allUserPicks.find { pick -> pick.user.name == user.name }
-
-            userPicks?.picks?.forEach { userPick ->
-                val game = games.find { game -> game.name == userPick.game }
-                if (game != null && game.result.equals(userPick.pick.trim(), ignoreCase = true)) {
-                    weeklyTotal.games.add(game)
-                }
-            }
-
-            results.add(weeklyTotal)
+            val picksForUser = allUserPicks.find(matchUserPicksForUser(user))
+            val correctGamesForUser = getCorrectPicksForUser(user, picksForUser, games)
+            results.add(correctGamesForUser)
         }
 
         return results
     }
+
+    private fun getCorrectPicksForUser(
+        user: UserDTO,
+        userPicks: UserPicksDTO?,
+        games: ArrayList<GameDTO>
+    ): UserWeekTotalDTO {
+        val correctPicks = UserWeekTotalDTO(user)
+        userPicks?.picks?.forEach { userPick ->
+            addGameForMatchingPick(correctPicks, games, userPick)
+        }
+        return correctPicks
+    }
+
+    private fun matchUserPicksForUser(user: UserDTO) = { pick: UserPicksDTO -> pick.user.name == user.name }
+
+    private fun addGameForMatchingPick(
+        correctPicks: UserWeekTotalDTO,
+        games: ArrayList<GameDTO>,
+        userPick: PickDTO
+    ) {
+        val game = games.find(gameForPick(userPick))
+        if (gameMatchesPick(game, userPick.pick)) {
+            correctPicks.games.add(game!!)
+        }
+    }
+
+    private fun gameForPick(userPick: PickDTO) = { game: GameDTO -> game.name == userPick.game }
+
+    private fun gameMatchesPick(game: GameDTO?, pick: String) =
+        game != null && game.result.equals(pick.trim(), ignoreCase = true)
 }
