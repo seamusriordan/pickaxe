4b3bcadfc8849f72e7668eedda8137a2b350b1ac
Seamus Riordan <seamus@riordan.io>
2020-05-18 08:42:26 -0500

    Integration tests for partial game data

--- e2e/cypress/integration/games_from_api.js
+++ e2e/cypress/integration/games_from_api.js
@@ -9,10 +9,37 @@ describe('games comes from api', () => {
     });
 
     it('completed game is set', () => {
-        cy.get('#result-0').should("contain", "NE")
+        cy.get('.game-cell')
+            .then(findIndexOfGame("NYG@NE"))
+            .then(i =>
+                cy.get(`#result-${i}`).contains("NE")
+            )
     });
 
     it('incomplete game is not set', () => {
-        cy.get('#result-1').should("be.empty")
+        cy.get('.game-cell')
+            .then(findIndexOfGame("CAR@TB"))
+            .then(i =>
+                cy.get(`#result-${i}`).should("be.empty")
+            )
+    });
+
+    it('game without game details id is not set', () => {
+        cy.get('.game-cell')
+            .then(findIndexOfGame("CIN@BAL"))
+            .then(i =>
+                cy.get(`#result-${i}`).should("be.empty")
+            )
     });
 })
+
+function findIndexOfGame(game) {
+    return (elements) => {
+        for (let i = 0; i < elements.length; i++) {
+            if (elements.get(i).innerText === game) {
+                return i;
+            }
+        }
+        return -1
+    };
+}
