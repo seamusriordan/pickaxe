700f2090faed7d97dae0af7e8a04ebea951ba947
Seamus Riordan <seamus@riordan.io>
2020-06-04 07:22:23 -0500

    Wire up leader query in graph ql

--- server/src/main/kotlin/db/LeaderQuery.kt
+++ server/src/main/kotlin/db/LeaderQuery.kt
@@ -1,8 +1,11 @@
 package db
 
 import dto.LeaderDTO
+import graphql.schema.DataFetcher
+import graphql.schema.DataFetchingEnvironment
 
-class LeaderQuery(private var weeksQuery: WeeksQuery, private var weekTotalQuery: UserWeekTotalQuery) {
+class LeaderQuery(private var weeksQuery: WeeksQuery, private var weekTotalQuery: UserWeekTotalQuery) :
+    DataFetcher<List<LeaderDTO>> {
     fun get(): List<LeaderDTO> {
         val firstWeek = weeksQuery.get().first().name
         val leaders = weekTotalQuery.get(firstWeek).map {
@@ -13,14 +16,18 @@ class LeaderQuery(private var weeksQuery: WeeksQuery, private var weekTotalQuery
             val weekResults = weekTotalQuery.get(week.name)
             val mostWon = weekResults.map { results -> results.total }.max()
 
-            leaders.forEach{ leader ->
+            leaders.forEach { leader ->
                 val leaderResult = weekResults.first { it.user.name == leader.name }
-                if(leaderResult.total == mostWon) {
+                if (leaderResult.total == mostWon) {
                     leader.correctWeeks += 1
                 }
                 leader.correctPicks += leaderResult.total
             }
         }
         return leaders
+    }
+
+    override fun get(environment: DataFetchingEnvironment?): List<LeaderDTO> {
+        return get()
     }
 }
