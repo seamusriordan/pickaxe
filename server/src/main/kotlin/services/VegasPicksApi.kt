4183ea21a2f10fe757f37de806ea07698ec7a207
Seamus Riordan <seamus@riordan.io>
2020-06-28 08:21:50 -0500

    Start parsing with Jsoup

--- server/src/main/kotlin/services/VegasPicksApi.kt
+++ server/src/main/kotlin/services/VegasPicksApi.kt
@@ -1,8 +1,10 @@
 package services
 
 import dto.PickWithSpreadDTO
+import org.jsoup.Jsoup
 import java.io.InputStreamReader
 import java.net.URL
+import java.util.stream.IntStream.range
 
 class VegasPicksApi(private val url: URL) {
     fun getVegasPicks(): List<PickWithSpreadDTO> {
@@ -10,12 +12,68 @@ class VegasPicksApi(private val url: URL) {
         val stream = connection.getInputStream()
         val response = InputStreamReader(stream).readText()
 
-        if(response != ""){
-            return listOf(
-                PickWithSpreadDTO("PIT@DAL", "DAL", -1.5)
+        return parseResponseToGames(response)
+    }
+
+    private fun parseResponseToGames(response: String): List<PickWithSpreadDTO> {
+        val document = Jsoup.parse(response)
+
+        val gameCells = document.select("td.gameCell")
+        val oddsCells = document.select("td.gameCell + td.oddsCell + td.oddsCell")
+
+        val picks = mutableListOf<PickWithSpreadDTO>()
+        for (i in range(0, gameCells.size)) {
+            val gameCell = gameCells[i]
+            val oddsCell = oddsCells[i]
+
+            val teams = gameCell.select(".tabletext").map { cell -> translateToTeamAbbrev(cell.text()) }
+            val oddsString = oddsCell.text()
+
+            picks.add(
+                PickWithSpreadDTO(teams[0] + "@" + teams[1], "CHI", -10.5)
             )
+
         }
 
-        return listOf()
+        return picks
+    }
+
+    private fun translateToTeamAbbrev(longTeamName: String): String {
+        val translationMap = mapOf(
+            "Arizona" to "ARI",
+            "Atlanta" to "ATL",
+            "Baltimore" to "BAL",
+            "Buffalo" to "BUF",
+            "Carolina" to "CAR",
+            "Cincinnati" to "CIN",
+            "Chicago" to "CHI",
+            "Cleveland" to "CLE",
+            "Dallas" to "DAL",
+            "Denver" to "DEN",
+            "Detroit" to "DET",
+            "Green Bay" to "GB",
+            "Houston" to "HOU",
+            "Indianapolis" to "IND",
+            "Jacksonville" to "JAX",
+            "Kansas City" to "KC",
+            "Las Vegas" to "LAV",
+            "L.A. Chargers" to "LAC",
+            "L.A. Rams" to "LAR",
+            "Miami" to "MIA",
+            "Minnesota" to "MIN",
+            "New England" to "NE",
+            "New Orleans" to "NO",
+            "N.Y. Giants" to "NYG",
+            "N.Y. Jets" to "NYJ",
+            "Philadelphia" to "PHI",
+            "Pittsburgh" to "PIT",
+            "San Francisco" to "SF",
+            "Seattle" to "SEA",
+            "Tampa Bay" to "TB",
+            "Tennessee" to "TEN",
+            "Washington" to "WAS"
+        )
+
+        return translationMap[longTeamName] ?: longTeamName
     }
 }
--- server/src/test/kotlin/services/VegasPicksApiTest.kt
+++ server/src/test/kotlin/services/VegasPicksApiTest.kt
@@ -8,37 +8,29 @@ import java.net.HttpURLConnection
 import java.net.URL
 
 class VegasPicksApiTest {
-    private val sampleRowPitAtDal = "<tr>\n" +
-            "    <td class=\"viCellBg1 cellTextNorm cellBorderL1 gameCell\">\n" +
-            "        <span class=\"cellTextHot\">08/06  8:00 PM</span><br>\n" +
-            "        <a class=\"tabletext\">Pittsburgh</a>\n" +
-            "        <a class=\"tabletext\">Dallas</a>\n" +
-            "    </td>\n" +
-            "    <td class=\"viCellBg1 cellTextNorm cellBorderL1 center_text nowrap oddsCell\">\n" +
-            "        <a class=\"cellTextNorm\">\n" +
-            "            &nbsp;<br>-3&nbsp;-10<br>40u-10\n" +
-            "        </a>\n" +
-            "    </td>\n" +
-            "    <td class=\"viCellBg1 cellTextNorm cellBorderL1 center_text nowrap oddsCell\">\n" +
-            "        <a class=\"cellTextNorm\">\n" +
-            "            &nbsp;<br>34u-10<br>-1&frac12;&nbsp;-10\n" +
-            "        </a>\n" +
-            "    </td>\n" +
-            "</tr>\n"
+    private val sampleRow = buildSampleRow(
+        VegasData(
+            "Pittsburgh",
+            "Dallas",
+            "&nbsp;<br>34u-10<br>-1&frac12;&nbsp;-10"
+        )
+    )
 
 
-    @Test
-    fun getVegasPickWithNotDataReturnsNoGames() {
-        val handler = MockURLStreamHandler
-        val picksUrl = URL("https://picksendpoint")
-        val mockPicksConnection = mockkClass(HttpURLConnection::class)
+    private val handler = MockURLStreamHandler
+    private val picksUrl = URL("https://picksendpoint")
+    private val mockPicksConnection = mockkClass(HttpURLConnection::class)
 
+    init {
         handler.setConnection(picksUrl, mockPicksConnection)
 
         every { mockPicksConnection.requestMethod = "GET" } returns Unit
         every { mockPicksConnection.doOutput = true } returns Unit
-        every { mockPicksConnection.inputStream } returns "".byteInputStream()
+    }
 
+    @Test
+    fun getVegasPickWithNotDataReturnsNoGames() {
+        every { mockPicksConnection.inputStream } returns "".byteInputStream()
         val picksApi = VegasPicksApi(picksUrl)
 
         val picks = picksApi.getVegasPicks()
@@ -47,25 +39,108 @@ class VegasPicksApiTest {
     }
 
     @Test
-    fun getVegasPickWithSampleRowReturnsGame() {
-        val handler = MockURLStreamHandler
-        val picksUrl = URL("https://picksendpoint")
-        val mockPicksConnection = mockkClass(HttpURLConnection::class)
+    fun teamWithBadNameTranslatesIdentically() {
+        every { mockPicksConnection.inputStream } returns buildSampleRow(
+            VegasData(
+                "Pittsburgh",
+                "Dertroid",
+                "<br>56&frac12;u-10<br>-10&nbsp;-10"
+            )
+        ).byteInputStream()
 
-        handler.setConnection(picksUrl, mockPicksConnection)
+        val picks = VegasPicksApi(picksUrl).getVegasPicks()
 
-        every { mockPicksConnection.requestMethod = "GET" } returns Unit
-        every { mockPicksConnection.doOutput = true } returns Unit
-        every { mockPicksConnection.inputStream } returns sampleRowPitAtDal.byteInputStream()
+        assertEquals("PIT@Dertroid", picks.first().game)
+    }
 
-        val picksApi = VegasPicksApi(picksUrl)
 
-        val picks = picksApi.getVegasPicks()
+    @Test
+    fun twoPicksInDataWillReturnTwoPicks() {
+        every { mockPicksConnection.inputStream } returns buildSampleRows(
+            listOf(
+                VegasData(
+                    "Pittsburgh",
+                    "Dallas",
+                    "<br>56&frac12;u-10<br>-10&nbsp;-10"
+                ),
+                VegasData(
+                    "Houston",
+                    "Kansas City",
+                    "<br>56&frac12;u-10<br>-10&nbsp;-10"
+                )
+            )
+        ).byteInputStream()
+
+        val picks = VegasPicksApi(picksUrl).getVegasPicks()
+
+        assertEquals(2, picks.size)
+        assertEquals("PIT@DAL", picks[0].game)
+        assertEquals("HOU@KC", picks[1].game)
+    }
+
+    @Test
+    fun getVegasPickWithSampleRowPitAtDalReturnsGame() {
+        every { mockPicksConnection.inputStream } returns sampleRow.byteInputStream()
+
+        val picks = VegasPicksApi(picksUrl).getVegasPicks()
 
         assertEquals(1, picks.size)
         val pick = picks.first()
         assertEquals("PIT@DAL", pick.game)
         assertEquals("DAL", pick.pick)
         assertEquals(-1.5, pick.spread)
+    }
+
+    @Test
+    fun getVegasPickWithSampleRowHouAtKcReturnsGame() {
+        every { mockPicksConnection.inputStream } returns buildSampleRow(
+            VegasData(
+                "Houston",
+                "Kansas City",
+                "<br>56&frac12;u-10<br>-10&nbsp;-10"
+            )
+        ).byteInputStream()
+
+        val picks = VegasPicksApi(picksUrl).getVegasPicks()
+
+        assertEquals(1, picks.size)
+        val pick = picks.first()
+        assertEquals("HOU@KC", pick.game)
+        assertEquals("KC", pick.pick)
+        assertEquals(-10.5, pick.spread)
+    }
+
+    private inner class VegasData(val awayTeam: String, val homeTeam: String, val spread: String)
+
+    private fun buildSampleRow(vegasData: VegasData): String {
+        return wrapWithTable(
+            "<tr>\n" +
+                    "    <td class=\"viCellBg1 cellTextNorm cellBorderL1 gameCell\">\n" +
+                    "        <span class=\"cellTextHot\">08/06  8:00 PM</span><br>\n" +
+                    "        <a class=\"tabletext\">${vegasData.awayTeam}</a>\n" +
+                    "        <a class=\"tabletext\">${vegasData.homeTeam}</a>\n" +
+                    "    </td>\n" +
+                    "    <td class=\"viCellBg1 cellTextNorm cellBorderL1 center_text nowrap oddsCell\">\n" +
+                    "        <a class=\"cellTextNorm\">\n" +
+                    "            &nbsp;<br>-3&nbsp;-10<br>40u-10\n" +
+                    "        </a>\n" +
+                    "    </td>\n" +
+                    "    <td class=\"viCellBg1 cellTextNorm cellBorderL1 center_text nowrap oddsCell\">\n" +
+                    "        <a class=\"cellTextNorm\">\n" +
+                    "            ${vegasData.spread}\n" +
+                    "        </a>\n" +
+                    "    </td>\n" +
+                    "</tr>\n"
+        )
+    }
+
+    private fun wrapWithTable(inner: String): String {
+        return "<table>$inner</table>"
+    }
+
+    private fun buildSampleRows(vegasData: List<VegasData>): String {
+        return wrapWithTable(
+            vegasData.map { buildSampleRow(it) }.reduce { allRows, datum -> "$allRows${datum}" }
+        )
     }
 }
