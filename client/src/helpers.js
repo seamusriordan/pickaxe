8fd14d304ea2c6c6116f2edc0ba6ba010cac6616
Seamus Riordan <seamus@riordan.io>
2020-09-26 06:48:57 -0500

    Remove environment variables in client and replace with window locations

--- client/src/helpers.js
+++ client/src/helpers.js
@@ -1,47 +1,17 @@
-export function getWebsocketHostname() {
-    return process.env.REACT_APP_GRAPHQL_SERVER ?
-        process.env.REACT_APP_GRAPHQL_SERVER :
-        "localhost";
-}
-
-export function getWebsocketPort() {
-    return process.env.REACT_APP_GRAPHQL_PORT ?
-        process.env.REACT_APP_GRAPHQL_PORT :
-        "8080";
-}
-
 export function getWebsocketProtocol() {
-    return process.env.REACT_APP_GRAPHQL_HTTPS ?
+    return window.location.protocol === 'https'?
         "wss" :
         "ws";
 }
 
 export function buildWebsocketUri() {
     return getWebsocketProtocol() + '://' +
-        getWebsocketHostname() + ':' + getWebsocketPort() +
+        window.location.host +
         '/pickaxe/updateNotification';
 }
 
-export function getGraphqlServer() {
-    return process.env.REACT_APP_GRAPHQL_SERVER ?
-        process.env.REACT_APP_GRAPHQL_SERVER :
-        "localhost";
-}
-
-export function getGraphqlPort() {
-    return process.env.REACT_APP_GRAPHQL_PORT ?
-        process.env.REACT_APP_GRAPHQL_PORT :
-        "8080";
-}
-
-export function getGraphqlProtocol() {
-    return process.env.REACT_APP_GRAPHQL_HTTPS ?
-        "https" :
-        "http";
-}
-
 export function buildGraphqlUri() {
-    return getGraphqlProtocol() + '://' +
-        getGraphqlServer() + ':' + getGraphqlPort() +
+    return window.location.protocol + '://' +
+        window.location.host +
         '/pickaxe/graphql';
 }
